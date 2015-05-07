8473cc051cbb70358dfd01dcadee6d24312c705f
t3jwm <jmccabe@dunkielsaunders.com>
2015-05-06 21:53:21 -0400

    fullpath add

--- fullpath.php
+++ fullpath.php
@@ -1,5 +1,3 @@
-<?php
-$dir = dirname(__FILE__);
-echo "<p>Full path to this dir: " . $dir . "</p>";
-echo "<p>Full path to a .htpasswd file in this dir: " . $dir . "/.htpasswd" . "</p>";
+< ?php
+phpinfo();
 ?>
