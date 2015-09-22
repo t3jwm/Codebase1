e1e042039a8fdf0850dd6831c98ab3ac48c87939
tamsasa <jimmysscentaur@gmail.com>
2015-09-23 00:32:58 +0700

    add-save

--- src/Acme/DemoBundle/Controller/DefaultController.php
+++ src/Acme/DemoBundle/Controller/DefaultController.php
@@ -150,10 +150,6 @@ class DefaultController extends Controller
 	
 				$em->persist($file);
 				$em->flush();
-				$response = new Response(json_encode(array('Term' => 'termToFixError')));
-				$response->headers->set('Content-Type', 'application/json');
-		
-				return $response;
 		
 		}else{			
 			
@@ -167,10 +163,82 @@ class DefaultController extends Controller
         	echo "chuanbi";
         	echo "<pre>"; var_dump($product); die;
 
-	        if($product == 0){
-	            return $this->render('AcmeDemoBundle:User:emptygallery.html.twig');
-	        }		
+		/*
+				 ----------------------------------------------------------------
+				 FILE MANAGER : 1.0.3 : 2014-11-04
+				 ----------------------------------------------------------------
+				 SETUP:
+				 chmod 777 ./media/upload/
+				 ----------------------------------------------------------------
+				 */
+				
+				$basedir = "/media/upload";
+				$userdir = "media"; // logged in username
+				$document = $product->path;
+				$targetdir = "{$userdir}";
+				//$created = Array();
+				//
+				@mkdir("{$targetdir}"); // create target dir}
+				
+				/* MIME TYPES (supported formats)
+				 -------------------------------------------------------- */
+				$mime = Array(
+						"sketch"=> "application/zip",
+						"json"=> "application/json",
+						"svg"=> "image/svg+xml",
+						"thumb.gif"=> "image/gif",
+						"thumb.jpeg"=> "image/jpeg",
+						"thumb.png"=> "image/png",
+						"gif"=> "image/gif",
+						"jpeg"=> "image/jpeg",
+						"png"=> "image/png"
+				);
+				
+				/* $_FILES (Binary upload)
+				 -------------------------------------------------------- */
+				$useFILES = isset($_FILES) && count($_FILES);
+				if ($useFILES) { // blob
+				
+					foreach ($_FILES as $key=>$value) {
+						$name = str_replace("_", ".", $key);
+						$ext = array_pop((explode(".", $name)));
+						if (!isset($mime[$ext])) continue;
+						$tmp_name = $_FILES[$key]["tmp_name"];
+						//$md5 = md5_file($tmp_name);
+						$filepath = "{$targetdir}/{$document}.{$ext}";
+						//$filepath = "{$targetdir}/{$md5}.{$ext}";
+						//$created[] = $filepath;
+						move_uploaded_file($tmp_name, $filepath);
+					}
+				}
+				
+				/* $_POST (UTF-8 upload)
+				 -------------------------------------------------------- */
+				$usePOST = isset($_POST) && count($_POST);
+					
+				if ($usePOST) { // string
+					foreach ($_POST as $key=>$value) {
+						$ext = str_replace("_", ".", $key);
+						if (!isset($mime[$ext])) continue;
+						list($type, $extension) = explode("/", $mime[$ext]);
+						/// Base64 decode.
+						if ($type === "image" && $ext !== "svg") {
+							$split = explode(",", $value);
+							$value = base64_decode($split[1]);
+						}
+						/// Put image data.
+						//$md5 = md5($value);
+						$filepath = "{$targetdir}/{$document}.{$ext}";
+						//$filepath = "{$targetdir}/{$md5}.{$ext}";
+						//$created[] = $filepath;
+						file_put_contents($filepath, $value);
+					}
+				}	        		
 		}
+				$response = new Response(json_encode(array('Term' => 'termToFixError')));
+				$response->headers->set('Content-Type', 'application/json');
+		
+				return $response;
 	}
 
 	public function downloadAction()
