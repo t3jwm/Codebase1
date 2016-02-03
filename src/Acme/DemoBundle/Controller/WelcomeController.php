9d8c9cb67b928c0f1f6accf06751d02a531aab63
jimmysscentaur <jimmysscentaur@gmail.com>
2016-02-03 21:57:30 +0700

    Fix error Homepage-login

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -9,20 +9,20 @@ class WelcomeController extends Controller {
 
     public function indexAction() {
         /*
-         * The action's view can be rendered using render() method
+         * The action"s view can be rendered using render() method
          * or @Template annotation as demonstrated in DemoController.
          *
          */
         $session = $this->getRequest()->getSession();
-        if ($session->has('login')) {
-            $login = $session->get('login');
+        if ($session->has("login")) {
+            $login = $session->get("login");
             $userName = $login->getUsername();
 
 
-            return $this->render('AcmeDemoBundle:Demo:homepage.html.twig', array('name' => $userName));
+            return $this->render("AcmeDemoBundle:Demo:homepage.html.twig", array("name" => $userName));
         }
         return $this->render(
-                        'AcmeDemoBundle:Demo:homepage.html.twig', array('name' => ''));
+                        "AcmeDemoBundle:Demo:homepage.html.twig", array("name" => ""));
     }
 
     public function appAction() {
@@ -32,94 +32,94 @@ class WelcomeController extends Controller {
             $userName = $login->getUsername();
             return $this->render("AcmeDemoBundle:Demo:app.html.twig", array("name" => $userName, "code" => "", "idImage" => ""));
         } else {
-            return $this->render("AcmeDemoBundle:Demo:homepage.html.twig");
+            return $this->render("AcmeDemoBundle:Demo:homepage.html.twig", array("name" => ""));
         }
     }
 
     public function contactusAction() {
         /*
-         * The action's view can be rendered using render() method
+         * The action"s view can be rendered using render() method
          * or @Template annotation as demonstrated in DemoController.
          *
          */
         $session = $this->getRequest()->getSession();
-        if ($session->has('login')) {
-            $login = $session->get('login');
+        if ($session->has("login")) {
+            $login = $session->get("login");
             $userName = $login->getUsername();
 
 
-            return $this->render('AcmeDemoBundle:Demo:contactus.html.twig', array('name' => $userName));
+            return $this->render("AcmeDemoBundle:Demo:contactus.html.twig", array("name" => $userName));
         }
         return $this->render(
-                        'AcmeDemoBundle:Demo:contactus.html.twig', array('name' => ''));
+                        "AcmeDemoBundle:Demo:contactus.html.twig", array("name" => ""));
     }
 
     public function termsAction() {
         /*
-         * The action's view can be rendered using render() method
+         * The action"s view can be rendered using render() method
          * or @Template annotation as demonstrated in DemoController.
          *
          */
         $session = $this->getRequest()->getSession();
-        if ($session->has('login')) {
-            $login = $session->get('login');
+        if ($session->has("login")) {
+            $login = $session->get("login");
             $userName = $login->getUsername();
 
 
-            return $this->render('AcmeDemoBundle:Demo:terms.html.twig', array('name' => $userName));
+            return $this->render("AcmeDemoBundle:Demo:terms.html.twig", array("name" => $userName));
         }
         return $this->render(
-                        'AcmeDemoBundle:Demo:terms.html.twig', array('name' => ''));
+                        "AcmeDemoBundle:Demo:terms.html.twig", array("name" => ""));
     }
 
     public function privacyAction() {
         /*
-         * The action's view can be rendered using render() method
+         * The action"s view can be rendered using render() method
          * or @Template annotation as demonstrated in DemoController.
          *
          */
         $session = $this->getRequest()->getSession();
-        if ($session->has('login')) {
-            $login = $session->get('login');
+        if ($session->has("login")) {
+            $login = $session->get("login");
             $userName = $login->getUsername();
 
 
-            return $this->render('AcmeDemoBundle:Demo:privacy.html.twig', array('name' => $userName));
+            return $this->render("AcmeDemoBundle:Demo:privacy.html.twig", array("name" => $userName));
         }
         return $this->render(
-                        'AcmeDemoBundle:Demo:privacy.html.twig', array('name' => ''));
+                        "AcmeDemoBundle:Demo:privacy.html.twig", array("name" => ""));
     }
 
     public function faqAction() {
         /*
-         * The action's view can be rendered using render() method
+         * The action"s view can be rendered using render() method
          * or @Template annotation as demonstrated in DemoController.
          *
          */
         $session = $this->getRequest()->getSession();
-        if ($session->has('login')) {
-            $login = $session->get('login');
+        if ($session->has("login")) {
+            $login = $session->get("login");
             $userName = $login->getUsername();
 
 
-            return $this->render('AcmeDemoBundle:Demo:faq.html.twig', array('name' => $userName));
+            return $this->render("AcmeDemoBundle:Demo:faq.html.twig", array("name" => $userName));
         }
         return $this->render(
-                        'AcmeDemoBundle:Demo:faq.html.twig', array('name' => ''));
+                        "AcmeDemoBundle:Demo:faq.html.twig", array("name" => ""));
     }
 
     public function openImageDashAction($code) {
 
         $session = $this->getRequest()->getSession();
-        if (!$session->has('login')) {
-            return $this->redirect($this->generateUrl('account_register'));
+        if (!$session->has("login")) {
+            return $this->redirect($this->generateUrl("account_register"));
         }
         $em = $this->getDoctrine()->getManager();
         $repository = $this->getDoctrine()->getRepository("AcmeDemoBundle:File");
         $file = $repository->findOneById($code);
         if (($file != null) && ($file->userId == $session->get("login")->getUserId())) {            
-            $filepath_dash = '/' . $file->root . '/' . $file->path . '.json';
-            return $this->render('AcmeDemoBundle:Demo:app.html.twig', array('name' => $session->get("login")->getUserName(), 'code' => $filepath_dash, 'idImage' => $code));
+            $filepath_dash = "/" . $file->root . "/" . $file->path . ".json";
+            return $this->render("AcmeDemoBundle:Demo:app.html.twig", array("name" => $session->get("login")->getUserName(), "code" => $filepath_dash, "idImage" => $code));
         }else{
             echo "You can not access this image"; exit;
         }
