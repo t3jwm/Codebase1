9290819f1695665918535cfbcba7cc21d83c831a
Justin <jwmccabe1@gmail.com>
2016-05-12 16:41:58 -0400

    button fixes

--- web/bundles/sketchapp-handel/js/img-sketchapp-handel.js
+++ web/bundles/sketchapp-handel/js/img-sketchapp-handel.js
@@ -70,8 +70,8 @@ var ImageSketchapp = {
             }
         });
     },
-    openImageOnSketchApp: function ($imgId, $id) {
-        window.location.replace("/apppage/" + $id);
+    openImageOnSketchApp: function ($imgId) {
+        window.location.replace("/apppage/" + (window.btoa(' + $imgId + '),'+ $imgId +' )" src="' + $rscImg + '");
     },
     showLargeImg: function ($imgId, $rscImg, $nameImg, $id, $filePath) {
         bootbox.dialog({
@@ -110,7 +110,7 @@ var ImageSketchapp = {
                     label: " Open in Sketchapp",
                     className: "btn btn-info btn-xs fa fa-paint-brush",
                     callback: function () {
-                        ImageSketchapp.openImageOnSketchApp($imgId, $id);
+                        ImageSketchapp.openImageOnSketchApp($imgId);
                     }
                 }
             }
