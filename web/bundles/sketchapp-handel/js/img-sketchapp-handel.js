a8ed5a20caa2968d7b44f3a94594181fbaff4810
Justin <jwmccabe1@gmail.com>
2016-05-12 15:16:20 -0400

    button fixes

--- web/bundles/sketchapp-handel/js/img-sketchapp-handel.js
+++ web/bundles/sketchapp-handel/js/img-sketchapp-handel.js
@@ -110,7 +110,7 @@ var ImageSketchapp = {
                     label: " Open in Sketchapp",
                     className: "btn btn-info btn-xs fa fa-paint-brush",
                     callback: function () {
-                        ImageSketchapp.openImageOnSketchApp($imgId, $rscImg);
+                        ImageSketchapp.openImageOnSketchApp(window.btoa(' + $imgId + '),'+ $imgId +' )" src="' + $rscImg + '";
                     }
                 }
             }
